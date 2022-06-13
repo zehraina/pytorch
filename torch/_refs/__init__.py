7b51730b77edc82d9f4caa25ad3d9f9286fec109
Syed Tousif Ahmed <syeahmed@nvidia.com>
2022-06-13 08:53:47 -0700

    [primTorch] Brings back disabling scalar support that were dropped during rebasing

--- torch/_refs/__init__.py
+++ torch/_refs/__init__.py
@@ -835,6 +835,8 @@ gcd = _make_elementwise_binary_reference(
     prims.gcd,
     type_promotion_kind=ELEMENTWISE_TYPE_PROMOTION_KIND.DEFAULT,
     aten_op=torch.ops.aten.gcd,
+    supports_lhs_python_scalar=False,
+    supports_rhs_python_scalar=False,
 )
 
 # TODO: add docstring
@@ -937,6 +939,8 @@ lcm = _make_elementwise_binary_reference(
     _lcm,
     type_promotion_kind=ELEMENTWISE_TYPE_PROMOTION_KIND.DEFAULT,
     aten_op=torch.ops.aten.lcm,
+    supports_lhs_python_scalar=False,
+    supports_rhs_python_scalar=False,
 )
 
 
